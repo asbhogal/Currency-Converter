30368a3c1de9b9ad0354847bc55fc6f046eab1ff
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-11 22:22:26 +0100

    add. to script incl. array loop

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const dropDownList = document.querySelector('.select-from-choice');
+
+for (let i = 0; i < dropDownList.length; i++) {
+    for (base_code in country_code) {
+        console.log(base_code);
+    }
+}
