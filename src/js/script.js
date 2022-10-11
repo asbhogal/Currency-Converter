0ed6c35ac4b7f64397716220df98b5b31526f7de
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-11 22:39:54 +0100

    loop array with optionTag via drop-down list added

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
-const dropDownList = document.querySelector('.select-from-choice');
+const dropDownList = document.querySelectorAll('.select-from-choice');
 
 for (let i = 0; i < dropDownList.length; i++) {
     for (base_code in country_code) {
-        console.log(base_code);
+        let optionTag = `<option value="$base_code}">${base_code}</option>`;
+        dropDownList[i].insertAdjacentHTML('beforeend', optionTag);
     }
 }
