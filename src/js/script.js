75f6cd9d106fc68d99be1350d2d597236d59f52f
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-12 15:16:53 +0100

    if statement modifications - template strings

--- src/js/script.js
+++ src/js/script.js
@@ -18,10 +18,11 @@ fetch(`${baseCurrencySrc}`, {
 
 for (let i = 0; i < dropDownList.length; i++) {
     for (base_code in country_code) {
+        let defaultChoice;
         if (i == 1) {
-            base_code = "GBP";
+            defaultChoice = base_code == "GBP" ? "defaultChoice" : "";
         }
-        let optionTag = `<option value="{$base_code}">${base_code}</option>`;
+        let optionTag = `<option value="${base_code}" ${defaultChoice}>${base_code}</option>`;
         dropDownList[i].insertAdjacentHTML('beforeend', optionTag);
     }
 }
