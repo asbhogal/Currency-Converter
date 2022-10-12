d53670d121e223cda6186bbc8880370d4d3deca7
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-12 22:53:31 +0100

    switched hardcoded js variable call w/ fetch GET from API

--- src/js/old-code.js
+++ src/js/old-code.js
@@ -40,3 +40,26 @@ if (i == 0) {
 } else if (i == 1) {
     base_code = "GBP" 
 }
+
+
+                /*let USDCode = json.rates.GBP;
+                console.log(USDCode);*/
+
+                /*const base_code = "USD";
+                const result = json.rates[base_code];
+                console.log(result);*/
+
+/*for (let i = 0; i < dropDownList.length; i++) {
+    for (base_code in country_code) {
+        let selected;
+        
+        if (i == 0) {
+            selected = base_code == "USD" ? "selected" : "";
+        } else if (i == 1) {
+            selected = base_code == "GBP" ? "selected" : "";
+        }
+
+        let optionTag = `<option value="${base_code}" ${selected}>${base_code}</option>`;
+        dropDownList[i].insertAdjacentHTML('beforeend', optionTag);
+    }
+}*/
--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,28 @@
 const dropDownList = document.querySelectorAll('.select-from-choice');
 const baseCurrencySrc = "https://open.er-api.com/v6/latest";
 
-for (let i = 0; i < dropDownList.length; i++) {
-    for (base_code in country_code) {
-        let selected;
-        
-        if (i == 0) {
-            selected = base_code == "USD" ? "selected" : "";
-        } else if (i == 1) {
-            selected = base_code == "GBP" ? "selected" : "";
-        }
-
-        let optionTag = `<option value="${base_code}" ${selected}>${base_code}</option>`;
-        dropDownList[i].insertAdjacentHTML('beforeend', optionTag);
-    }
-}
-
 fetch(`${baseCurrencySrc}`, {
     method: "GET",
     }).then((Response) => {
         if(Response.ok) {
             Response.json().then((json) => {
-                console.log(json);
+                console.log(json.rates);
 
-                let USDCode = json.rates.GBP;
-                console.log(USDCode);
+                for (base_code in json.rates) {
+                    for (let i = 0; i < dropDownList.length; i++) {
+                        let selected;
+                        
+                        if (i == 0) {
+                            selected = base_code == "USD" ? "selected" : "";
+                        } else if (i == 1) {
+                            selected = base_code == "GBP" ? "selected" : "";
+                        }
+                
+                        let optionTag = `<option value="${base_code}" ${selected}>${base_code}</option>`;
+                        dropDownList[i].insertAdjacentHTML('beforeend', optionTag);
+                    }
+                }
+                
             })
         }
     }).catch((error) => {
