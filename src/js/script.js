6114e069dfb7bc3be40af14b2a0d4a19dc8b2ec8
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-12 13:41:47 +0100

    modified if statement

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,27 @@
 const dropDownList = document.querySelectorAll('.select-from-choice');
+const baseCurrencySrc = "https://open.er-api.com/v6/latest";
+
+fetch(`${baseCurrencySrc}`, {
+    method: "GET",
+    }).then((Response) => {
+        if(Response.ok) {
+            Response.json().then((json) => {
+                console.log(json);
+
+                let USDCode = json.rates.GBP;
+                console.log(USDCode);
+            })
+        }
+    }).catch((error) => {
+        console.log(error);
+});
 
 for (let i = 0; i < dropDownList.length; i++) {
     for (base_code in country_code) {
-        let optionTag = `<option value="$base_code}">${base_code}</option>`;
+        if (i == 1) {
+            base_code = "GBP";
+        }
+        let optionTag = `<option value="{$base_code}">${base_code}</option>`;
         dropDownList[i].insertAdjacentHTML('beforeend', optionTag);
     }
 }
